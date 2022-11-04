2504b2fafa1e6f193af4fbdb7d358f6a5d4c3bea
cocoyellow <102589135+cocoyellow@users.noreply.github.com>
2022-11-04 14:19:30 +0100

    initial work mock up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route, Routes } from 'react-router-dom' 
+
+// let obj = {
+//   repoName: res.data.full_name,
+//   createdAt: res.data.createdAt,
+//   languages: res.data.language
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<IndexPage/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+function IndexPage() {
+
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    axios.defaults.baseURL = "https://api.github.com"
+    axios.get('/users/cocoyellow/repos').then(res => {
+      setRepos(res.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }, [])
+
+  return(
+    <div className='page'>
+      <div className='profile'>
+        Hi, I'm Israel Albright Chigozirim.
+        Front End Developer and Designer.
+      </div>
+      { repos.map((repo) => (
+        <ListRepoComponent repo={repo} key={repo.id}/>
+      ))}
+    </div>
+  )
+}
+
+function ListRepoComponent({ repo }) {
+  return (
+    <div className='repo-component-card'>
+      <div className='title'>
+        { repo.full_name }
+      </div>
+      <div className='divider'></div>
+      <div className='body'>
+        <div>{repo.createdAt}</div>
+        <div>{repo.language}</div>
+      </div>
+    </div>
+  )
+}
